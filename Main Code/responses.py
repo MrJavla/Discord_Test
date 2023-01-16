45c9eb098c13cf755670ddd4761c81ff9741489c
Amin <amin.hakim.yahya@gmail.com>
2023-01-16 17:41:17 +0800

    Initial commit

---
+++ Main Code/responses.py
@@ -0,0 +1,14 @@
+import random
+
+def handle_response(message) -> str:
+    p_message = message.lower()
+
+    if p_message == 'hello':
+        return 'shut up bitch!'
+
+    if p_message == 'roll':
+        return str(random.randint(1,6))
+
+
+
+
